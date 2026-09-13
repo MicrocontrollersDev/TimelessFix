1d5716dd778234938119f5705bf6d84c16cb2898
MicrocontrollersDev <microcontrollers.dev@pm.me>
2026-09-12 17:46:55 -0700

    add modern camera

--- src/main/java/dev/rdh/timelessfix/mixin/bugfix/EntityRendererMixin.java
+++ src/main/java/dev/rdh/timelessfix/mixin/bugfix/EntityRendererMixin.java
@@ -3,8 +3,12 @@ package dev.rdh.timelessfix.mixin.bugfix;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.rdh.timelessfix.TimelessFix;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,5 +32,10 @@ public class EntityRendererMixin {
     @ModifyExpressionValue(method = "orientCamera", at = @At(value = "CONSTANT", args = "floatValue=-0.1F"))
     private float tf$fixParallax(float original) {
         return !TimelessFix.CONFIG.modernParallax || this.tf$renderingDebugCrosshair ? original : 0.05F;
+    }
+
+    @WrapOperation(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;rayTraceBlocks(Lnet/minecraft/util/Vec3;Lnet/minecraft/util/Vec3;)Lnet/minecraft/util/MovingObjectPosition;"))
+    private MovingObjectPosition tf$ignoreCollisionlessBlocks(WorldClient instance, Vec3 vec31, Vec3 vec32, Operation<MovingObjectPosition> original) {
+        return instance.rayTraceBlocks(vec31, vec32, false, true, true);
     }
 }
--- src/main/java/dev/rdh/timelessfix/mixin/bugfix/WorldMixin.java
+++ src/main/java/dev/rdh/timelessfix/mixin/bugfix/WorldMixin.java
@@ -1,6 +1,13 @@
 package dev.rdh.timelessfix.mixin.bugfix;
 
+import com.google.common.collect.Sets;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
@@ -11,10 +18,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Set;
+
 @Mixin(World.class)
 public abstract class WorldMixin {
     @Shadow public abstract boolean isAreaLoaded(BlockPos center, int radius, boolean allowEmpty);
     @Unique private int tf$range = 17;
+    @Unique private static final Set<Block> tf$ignoredBlock = Sets.newHashSet(Blocks.glass, Blocks.glass_pane, Blocks.stained_glass, Blocks.stained_glass_pane, Blocks.iron_bars);
 
     @Inject(method = "checkLightFor", at = @At(value = "INVOKE", target = "Lnet/minecraft/profiler/Profiler;startSection(Ljava/lang/String;)V", ordinal = 0))
     private void tf$updateRange(EnumSkyBlock lightType, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
@@ -29,5 +39,10 @@ public abstract class WorldMixin {
     @ModifyExpressionValue(method  = "checkLightFor", at = {@At(value = "CONSTANT", args = "intValue=17", ordinal = 1), @At(value = "CONSTANT", args = "intValue=17", ordinal = 2)})
     private int setVariableRange(int original) {
         return this.tf$range;
+    }
+
+    @WrapOperation(method = "rayTraceBlocks(Lnet/minecraft/util/Vec3;Lnet/minecraft/util/Vec3;ZZZ)Lnet/minecraft/util/MovingObjectPosition;", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getCollisionBoundingBox(Lnet/minecraft/world/World;Lnet/minecraft/util/BlockPos;Lnet/minecraft/block/state/IBlockState;)Lnet/minecraft/util/AxisAlignedBB;"))
+    private AxisAlignedBB tf$ignoreSpecialBlocks(Block instance, World worldIn, BlockPos pos, IBlockState state, Operation<AxisAlignedBB> original) {
+        return tf$ignoredBlock.contains(state.getBlock()) ? null : original.call(instance, worldIn, pos, state);
     }
 }
