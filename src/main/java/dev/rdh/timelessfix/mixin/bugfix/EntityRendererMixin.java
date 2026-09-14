869cf970b648b8900fb619c2ce7ab7861b31ae5a
MicrocontrollersDev <microcontrollers.dev@pm.me>
2026-09-13 17:23:58 -0700

    fix MC-105732

--- src/main/java/dev/rdh/timelessfix/mixin/bugfix/EntityRendererMixin.java
+++ src/main/java/dev/rdh/timelessfix/mixin/bugfix/EntityRendererMixin.java
@@ -3,8 +3,13 @@ package dev.rdh.timelessfix.mixin.bugfix;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.rdh.timelessfix.TimelessFix;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumWorldBlockLayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,5 +33,14 @@ public class EntityRendererMixin {
     @ModifyExpressionValue(method = "orientCamera", at = @At(value = "CONSTANT", args = "floatValue=-0.1F"))
     private float tf$fixParallax(float original) {
         return !TimelessFix.CONFIG.modernParallax || this.tf$renderingDebugCrosshair ? original : 0.05F;
+    }
+
+    @WrapOperation(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/EnumWorldBlockLayer;DILnet/minecraft/entity/Entity;)I", ordinal = 3))
+    private int tf$offsetTranslucents(RenderGlobal instance, EnumWorldBlockLayer blockLayerIn, double partialTicks, int pass, Entity entityIn, Operation<Integer> original) {
+        GlStateManager.doPolygonOffset(-1.0F, -1.0F);
+        GlStateManager.enablePolygonOffset();
+        int ret = original.call(instance, blockLayerIn, partialTicks, pass, entityIn);
+        GlStateManager.disablePolygonOffset();
+        return ret;
     }
 }
