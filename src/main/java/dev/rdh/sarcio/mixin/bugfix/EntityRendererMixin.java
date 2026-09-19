c3d15771c76a31bb621576bef94fe1c9bd28242a
rdh <me@rdh540.dev>
2026-09-19 16:40:00 -0400

    fix MC-58177
    Night vision rendered darker and orange when nearing light sources with brightness on moody

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
@@ -2,6 +2,8 @@ package dev.rdh.sarcio.mixin.bugfix;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
@@ -12,7 +14,9 @@ import net.minecraft.util.EnumWorldBlockLayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
@@ -31,5 +35,12 @@ public class EntityRendererMixin {
     @ModifyArg(method = "updateRenderer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;getLightBrightness(Lnet/minecraft/util/BlockPos;)F"))
     private BlockPos sarcio$fixSkyDarkening(BlockPos original) {
         return new BlockPos(this.mc.getRenderViewEntity().getPositionEyes(1.0F));
+    }
+
+    @Inject(method = "updateLightmap", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityPlayerSP;isPotionActive(Lnet/minecraft/potion/Potion;)Z"))
+    private void sarcio$clampBeforeNightVision(float partialTicks, CallbackInfo ci, @Local(ordinal = 11) LocalFloatRef red, @Local(ordinal = 12) LocalFloatRef green, @Local(ordinal = 13) LocalFloatRef blue) {
+        red.set(Math.min(red.get(), 1.0F));
+        green.set(Math.min(green.get(), 1.0F));
+        blue.set(Math.min(blue.get(), 1.0F));
     }
 }
