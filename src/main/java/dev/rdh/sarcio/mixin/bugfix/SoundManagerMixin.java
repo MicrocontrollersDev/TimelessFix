98d04aa4f54189a36b158644f028aa357abc642b
rdh <me@rdh540.dev>
2026-09-19 18:31:59 -0400

    silence unknown sound warnings

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/SoundManagerMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/SoundManagerMixin.java
@@ -6,7 +6,11 @@ import java.util.Set;
 import net.minecraft.client.audio.SoundManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,6 +18,14 @@ import paulscode.sound.SoundSystem;
 
 @Mixin(SoundManager.class)
 public class SoundManagerMixin {
+    @Redirect(
+        method = "playSound",
+        slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=Unable to play unknown soundEvent: {}")),
+        at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Lorg/apache/logging/log4j/Marker;Ljava/lang/String;[Ljava/lang/Object;)V", remap = false)
+    )
+    private void sarcio$silenceUnknownSound(Logger logger, Marker marker, String message, Object[] args) {
+    }
+
     @Unique
     private final Set<String> sarcio$pausedChannels = new HashSet<>();
 
