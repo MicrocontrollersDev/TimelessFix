420de28908262ab10806ce28e905267ee144b8a7
rdh <me@rdh540.dev>
2026-09-19 17:59:43 -0400

    silence scoreboard exceptions

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/ScoreboardMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/ScoreboardMixin.java
@@ -1,19 +1,41 @@
 package dev.rdh.sarcio.mixin.bugfix;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.scoreboard.Scoreboard;
 
 @Mixin(Scoreboard.class)
-public class ScoreboardMixin {
+public abstract class ScoreboardMixin {
+	@Shadow public abstract ScorePlayerTeam getTeam(String name);
+
 	@Inject(method = "removeTeam", at = @At("HEAD"), cancellable = true)
 	private void sarcio$dontRemoveNull(ScorePlayerTeam team, CallbackInfo ci) {
 		if (team == null) {
 			ci.cancel();
 		}
+	}
+
+	@Inject(method = "removeObjective", at = @At("HEAD"), cancellable = true)
+	private void sarcio$dontRemoveNullObjective(ScoreObjective objective, CallbackInfo ci) {
+		if (objective == null) {
+			ci.cancel();
+		}
+	}
+
+	@Inject(method = "createTeam", at = @At(value = "CONSTANT", args = "stringValue=A team with the name '"), cancellable = true)
+	private void sarcio$reuseExistingTeam(String name, CallbackInfoReturnable<ScorePlayerTeam> cir) {
+		cir.setReturnValue(this.getTeam(name));
+	}
+
+	@Inject(method = "removePlayerFromTeam", at = @At(value = "CONSTANT", args = "stringValue=Player is either on another team or not on any team. Cannot remove from team '"), cancellable = true)
+	private void sarcio$dontThrowOnStaleTeam(String player, ScorePlayerTeam team, CallbackInfo ci) {
+		ci.cancel();
 	}
 }
