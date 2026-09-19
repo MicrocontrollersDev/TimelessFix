0f3bf00d531d2af07ff523ae202126b340cffaf3
Microcontrollers <66657148+MicrocontrollersDev@users.noreply.github.com>
2026-09-19 00:34:48 -0700

    fix MC-51150 (#24)
    * fix MC-51150
    
    * less ahh injector
    
    ---------
    
    Co-authored-by: rdh <me@rdh540.dev>

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
@@ -1,18 +1,24 @@
 package dev.rdh.sarcio.mixin.bugfix;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumWorldBlockLayer;
-
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
+    @Shadow
+    private Minecraft mc;
+
     @WrapOperation(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/EnumWorldBlockLayer;DILnet/minecraft/entity/Entity;)I", ordinal = 3))
     private int sarcio$offsetTranslucents(RenderGlobal instance, EnumWorldBlockLayer blockLayerIn, double partialTicks, int pass, Entity entityIn, Operation<Integer> original) {
         GlStateManager.doPolygonOffset(-1.0F, -1.0F);
@@ -20,5 +26,10 @@ public class EntityRendererMixin {
         int ret = original.call(instance, blockLayerIn, partialTicks, pass, entityIn);
         GlStateManager.disablePolygonOffset();
         return ret;
+    }
+
+    @ModifyArg(method = "updateRenderer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;getLightBrightness(Lnet/minecraft/util/BlockPos;)F"))
+    private BlockPos sarcio$fixSkyDarkening(BlockPos original) {
+        return new BlockPos(this.mc.getRenderViewEntity().getPositionEyes(1.0F));
     }
 }
