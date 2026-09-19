07b385911cfc36ed79e07852e9709fd004cbe386
rdh <me@rdh540.dev>
2026-09-19 18:09:24 -0400

    fix water thing

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/BlockFluidRendererMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/BlockFluidRendererMixin.java
@@ -1,6 +1,9 @@
 package dev.rdh.sarcio.mixin.bugfix;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.BlockFluidRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(BlockFluidRenderer.class)
 public class BlockFluidRendererMixin {
     @ModifyExpressionValue(method = "renderFluid", at = @At(value = "CONSTANT", args = "floatValue=0.001F"))
-    private float sarcio$fixFluidGap(float original) {
-        return 0.0F;
+    private float sarcio$fixFluidGap(float original, @Local(argsOnly = true) IBlockState state) {
+        return state.getBlock().getMaterial() == Material.water ? -original : original;
     }
 }
--- src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
@@ -1,16 +1,10 @@
 package dev.rdh.sarcio.mixin.bugfix;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderGlobal;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumWorldBlockLayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,15 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EntityRendererMixin {
     @Shadow
     private Minecraft mc;
-
-    @WrapOperation(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderBlockLayer(Lnet/minecraft/util/EnumWorldBlockLayer;DILnet/minecraft/entity/Entity;)I", ordinal = 3))
-    private int sarcio$offsetTranslucents(RenderGlobal instance, EnumWorldBlockLayer blockLayerIn, double partialTicks, int pass, Entity entityIn, Operation<Integer> original) {
-        GlStateManager.doPolygonOffset(-0.1F, -0.1F);
-        GlStateManager.enablePolygonOffset();
-        int ret = original.call(instance, blockLayerIn, partialTicks, pass, entityIn);
-        GlStateManager.disablePolygonOffset();
-        return ret;
-    }
 
     @ModifyArg(method = "updateRenderer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;getLightBrightness(Lnet/minecraft/util/BlockPos;)F"))
     private BlockPos sarcio$fixSkyDarkening(BlockPos original) {
