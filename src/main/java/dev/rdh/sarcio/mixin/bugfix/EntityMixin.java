fbac149039dbd602dcdd1872c17a649737951889
rdh <me@rdh540.dev>
2026-09-19 16:47:30 -0400

    fix MC-94535
    Flying and holding CTRL really close to the ground, emits walking particles

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityMixin.java
@@ -1,6 +1,7 @@
 package dev.rdh.sarcio.mixin.bugfix;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,5 +11,10 @@ public class EntityMixin {
     @ModifyExpressionValue(method = "getBrightnessForRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isBlockLoaded(Lnet/minecraft/util/BlockPos;)Z"))
     private boolean sarcio$fixOobEntityBrightness(boolean original) {
         return true;
+    }
+
+    @WrapWithCondition(method = "spawnRunningParticles", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;createRunningParticles()V"))
+    private boolean sarcio$onlyRunningParticlesOnGround(Entity instance) {
+        return instance.onGround;
     }
 }
