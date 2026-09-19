ff0535170d1f5ec0715575e25a2477898f30537b
rdh <me@rdh540.dev>
2026-09-19 17:59:42 -0400

    skip twitch stream initialization

--- src/main/java/dev/rdh/sarcio/mixin/tweaks/MinecraftMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/tweaks/MinecraftMixin.java
@@ -3,13 +3,26 @@ package dev.rdh.sarcio.mixin.tweaks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.stream.IStream;
+import net.minecraft.client.stream.NullStream;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
+    @Shadow
+    private IStream stream;
+
+    @Inject(method = "initStream", at = @At("HEAD"), cancellable = true)
+    private void sarcio$skipTwitchInit(CallbackInfo ci) {
+        this.stream = new NullStream(null);
+        ci.cancel();
+    }
+
     @WrapWithCondition(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/EntityRenderer;renderStreamIndicator(F)V"))
     private boolean sarcio$removeStreamIndicator(EntityRenderer instance, float partialTicks) {
         return false;
